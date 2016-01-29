3bcdb4216fb0af6564548fa5cf2aef4877f3301a
Artem Vorotnikov <artem@vorotnikov.me>
2016-01-29 18:07:28 +0300

    Tests: +SprintfCompare

--- adapters/test_common.go
+++ adapters/test_common.go
@@ -2,6 +2,10 @@ package adapters
 
 import "fmt"
 
-func ErrorOut(expectation interface{}, result interface{}) string {
-	return fmt.Sprintf("\nExpected %+v\nReceived %+v\n", expectation, result)
+func ErrorOut(expectation, result interface{}) string {
+	return fmt.Sprintf("\nExpected %#v\nReceived %#v\n", expectation, result)
+}
+
+func SprintfCompare(expectation, result interface{}) bool {
+	return fmt.Sprintf("%#v", expectation) == fmt.Sprintf("%#v", result)
 }
